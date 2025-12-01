f153956e4c983e2c02a28541f00e5a1f3c7998fb
golasola6 <golasola672@gmail.com>
2025-12-01 17:57:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def Lazy_start():
     await initialize_clients()
     if ON_HEROKU:
         asyncio.create_task(ping_server())
-    b_users, b_chats , lz_verified = await db.get_banned()
+    b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
