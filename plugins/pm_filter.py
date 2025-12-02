b45381c96825511fd84f7dec7b587d9a0e492187
golasola6 <golasola672@gmail.com>
2025-12-02 18:03:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1071,7 +1071,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             except Exception as e:
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
         except Exception as e:
-            print(e)
+            print(f"error in pm_filter : {e}")
     
     elif query.data.startswith("sendfiles"):
         user = query.from_user.id
